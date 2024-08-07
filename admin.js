a4934e930a7eec3f6696a1a1d1485541ba40a7d2
bebush <bebu100@hotmail.com>
2024-08-07 13:26:35 -0300

    1.7

--- admin.js
+++ admin.js
@@ -1,12 +1,23 @@
 function showContent(tab) {
     var pedidosContent = document.getElementById('pedidos');
     var reservasContent = document.getElementById('reservas');
+    var pedidosTab = document.querySelector('.tab.pedidos');
+    var reservasTab = document.querySelector('.tab.reservas');
 
     if (tab === 'pedidos') {
         pedidosContent.style.display = 'block';
         reservasContent.style.display = 'none';
+        pedidosTab.classList.add('active');
+        reservasTab.classList.remove('active');
     } else if (tab === 'reservas') {
         pedidosContent.style.display = 'none';
         reservasContent.style.display = 'block';
+        pedidosTab.classList.remove('active');
+        reservasTab.classList.add('active');
     }
 }
+
+// Establecer el botón de pedidos como activo por defecto
+document.addEventListener('DOMContentLoaded', function() {
+    showContent('pedidos');
+});

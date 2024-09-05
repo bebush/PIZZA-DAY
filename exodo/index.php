9c7947cc4fd2defa47ac48f82736d9271b39878a
bebush <bebu100@hotmail.com>
2024-09-05 16:09:04 -0300

    exodo

---
+++ exodo/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title>pizzaday</title>
+</head>
+<body>
+    <div class="logo"><p> pizza day </p></div>
+    <div class="lista-opciones">
+        <div class="list-items" id="postre"><a href="postres.php">postres</a></div>
+        <div class="list-items" id="minutas-picadas"><a href="/minutas y picadas.html">minutas y picada</a></div>
+        <div class="list-items" id="platos-especiales"><a href="/platos especiales.html">platos especiales</a></div>
+        <div class="list-items" id="menu-infantil"><a href="/menu infantil.html">menu infantil</a></div>
+        <div class="list-items" id="bebidas"><a href="/bebidas.html">bebidas</a></div>
+        <div class="list-items" id="reservas"><a href="/reservas.html">reservas</a></div>
+        <div class="list-items" id="pizza-catering"><a href="/pizza day catering.html">pizza catering</a></div>
+    </div>
+    <div class="pie-main"> all righ reserv</div>
+</body>
+</html>
